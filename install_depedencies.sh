b611d7fe0a9b1b553ea3fdbf18b1d0cab91a0d5a
MrDvD <ma.labin@yandex.ru>
2025-02-11 20:51:27 +0300

    refactor: added script for depedencies installation

---
+++ install_depedencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This file contains depedencies which need to be installed for web application
+
+# Install dependencies
+npm install express
+
+echo "Dependencies installed successfully."
