1449bd020caeaec01797114623bb0d14832dda65
MrDvD <ma.labin@yandex.ru>
2025-02-26 19:01:36 +0300

    fix: updated logic of slider

--- public/js/script.js
+++ public/js/script.js
@@ -20,4 +20,25 @@ document.addEventListener("DOMContentLoaded", () => {
             rightText.style.transform = "translateX(-100%)";
         }
     });
+
+   // Automatic slider functionality
+   const rotator = document.querySelector(".rotator");
+   let currentIndex = 0;
+   const slides = rotator.children;
+   const totalSlides = slides.length;
+   function rotateSlides() {
+      currentIndex++;
+      if (currentIndex == totalSlides - 1) {
+         currentIndex = 0;
+         rotator.style.transition = "none"; // Disable transition for instant reset
+         rotator.style.transform = `translateX(-${currentIndex * 50}%)`;
+         setTimeout(rotateSlides, 0); // Immediately call rotateSlides to continue the loop
+      } else {
+         rotator.style.transition = "transform 0.5s ease-in-out"; // Smooth transition for slide change
+         rotator.style.transform = `translateX(-${currentIndex * 50}%)`;
+         setTimeout(rotateSlides, 2500); // Wait 2.5 seconds before rotating to the next slide
+      }
+   }
+
+   setTimeout(rotateSlides, 2500); // Start the automatic slider after 2.5 seconds
 });
